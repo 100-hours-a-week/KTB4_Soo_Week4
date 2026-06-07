bca2dbff99aadf288561d894257db95775dfe9c9
jisoo <0801ljs@naver.com>
2026-06-07 20:52:05 +0900

    feat: Post엔티티 내부에 생성 및 수정 시간 필드 추가

--- src/main/java/ktb/soo/project/domain/post/entity/Post.java
+++ src/main/java/ktb/soo/project/domain/post/entity/Post.java
@@ -2,6 +2,8 @@ package ktb.soo.project.domain.post.entity;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class Post {
     private Long id;
@@ -9,12 +11,16 @@ public class Post {
     private String title;
     private String content;
     private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updateAt;
 
     public Post(Long userId, String title, String content, String status) {
         this.userId = userId;
         this.title = title;
         this.content = content;
         this.status = status;
+        this.createdAt = LocalDateTime.now();
+        this.updateAt = LocalDateTime.now();
     }
 
     public void setId(Long id) {
@@ -25,6 +31,7 @@ public class Post {
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
+        this.updateAt = LocalDateTime.now();
     }
 
     // 임시저장 -> 게시글 발행
@@ -32,5 +39,6 @@ public class Post {
         this.title = title;
         this.content = content;
         this.status = "PUBLISHED";
+        this.updateAt = LocalDateTime.now();
     }
 }
