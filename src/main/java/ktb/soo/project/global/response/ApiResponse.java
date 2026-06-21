df0787ee87f0a55148f247c8837bbcd1eed69a99
jisoo <0801ljs@naver.com>
2026-06-21 16:56:20 +0900

    refactor: 상세 에러 안내를 위해 ApiResponse에 message 필드 추가

--- src/main/java/ktb/soo/project/global/response/ApiResponse.java
+++ src/main/java/ktb/soo/project/global/response/ApiResponse.java
@@ -4,12 +4,22 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class ApiResponse<T>{
     private final String code;
+    private final String message;
     private final T data;
 
+    private ApiResponse(String code, String message, T data){
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
     public static <T> ApiResponse<T> of(String code, T data) {
-        return new ApiResponse<>(code, data);
+        return new ApiResponse<>(code,"요청이 성공적으로 처리되었습니다.", data);
+    }
+
+    public static <T> ApiResponse<T> onFailure(String errorCode, String errorMessage) {
+        return new ApiResponse<>(errorCode, errorMessage, null);
     }
 }
